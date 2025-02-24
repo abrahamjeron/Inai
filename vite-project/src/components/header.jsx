e311b2115fec3b6829c3a13572822abaf9348180
abrahamjeron <abrahamjeron40@gmail.com>
2025-02-24 18:01:12 +0530

    worst file path handled waste fellow

--- vite-project/src/components/header.jsx
+++ vite-project/src/components/header.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [user, setUser] = useState(null);
+//   
 
   useEffect(() => {
     // Fetch user details from localStorage
@@ -19,16 +20,27 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <Link to="/" className="text-xl font-bold">
-        My App
+    <header className="text-[#bab5b5] p-4 flex justify-between items-center">
+      <Link to="/" className="text-[2rem] relative ml-[35px] top-2 font-bold tracking-tight bg-gradient-to-r from-gray-400 via-gray-600 to-gray-800 text-transparent bg-clip-text"
+>
+        InaiVibe
       </Link>
       <nav className="space-x-4">
-        <Link to="/">Home</Link>
         {user ? (
           <>
-            <span>Hello, {user.username}</span>
-            <button
+             <div className="flex space-x-4 relative right-[30px] top-2">
+                <div>
+                  <h4 className="font-bold text-black text-[1.3rem]  " >{user.username}</h4>
+                  <p className="text-[0.9rem] font-extralight text-[#98A1B8] " >{user.email}</p>
+                </div>
+
+                <img
+                  src={user.avatar}
+                  alt='profile'
+                  className='h-10 w-10 top-2 relative rounded-full object-cover '
+                />
+              </div>
+            {/* <button
               onClick={() => {
                 localStorage.removeItem('user');
                 setUser(null);
@@ -37,7 +49,7 @@ const Header = () => {
               className="bg-red-600 px-2 py-1 rounded"
             >
               Logout
-            </button>
+            </button> */}
           </>
         ) : (
           <>
