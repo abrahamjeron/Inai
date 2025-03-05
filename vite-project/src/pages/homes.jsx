7b02482ef2e3cf537b45991e8669d2fd1713fdb3
abrahamjeron <abrahamjeron40@gmail.com>
2025-03-05 17:22:42 +0530

    done some microinteractions

--- vite-project/src/pages/homes.jsx
+++ vite-project/src/pages/homes.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import io from "socket.io-client";
-import { Link } from "react-router-dom";
 import { Search } from "lucide-react";
 import ChatRoom from "../components/ChatRoom";
 
